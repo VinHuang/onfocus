d0ac93e955e651cb479d168b219a9e64e1859a37
VinHuang <shadowmirror@gmail.com>
2016-07-08 13:49:48 +0800

    first commit

---
+++ js/index.js
@@ -0,0 +1,7 @@
+function focustest() {
+  document.getElementById('alert').innerHTML = 'input on focus';
+}
+
+function blurtest() {
+  document.getElementById("alert").innerHTML = "input not on focus"
+}
